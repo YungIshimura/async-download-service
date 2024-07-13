20796fecf7782df864c6abc5441a65dafc469b6f
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2024-07-13 15:51:15 +0300

    Добавил логику скачивания архива

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM python:3.10.14
+
+RUN apt-get update && \
+    apt-get install -y \
+    zip\
+    build-essential \
+    python3-dev \
+    libpython3.11-dev \
+    gcc \
+    libssl-dev \
+    libffi-dev \
+    make \
+    && apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+COPY . /app/
+COPY requirements.txt .
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+EXPOSE 8080
+
+CMD [ "python", "server.py" ]
--- server.py
+++ server.py
@@ -1,9 +1,51 @@
 from aiohttp import web
 import aiofiles
+import asyncio
+import os
+from aiologger import Logger
+import time
+import argparse
 
 
 async def archive(request):
-    raise NotImplementedError
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = 'attachment; filename="images.zip"'
+    await response.prepare(request)
+
+    archive_hash = request.match_info.get('archive_hash')
+    directory = os.path.join(namespace.path, archive_hash)
+    
+    proc = await asyncio.create_subprocess_exec(
+        'zip', '-r9', 'images.zip', directory,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd=os.getcwd()
+    )
+    await proc.communicate()
+
+
+    zip_path = os.path.join(os.getcwd(), 'images.zip')
+
+    try:
+        start_time = time.time()
+        with open(zip_path, 'rb') as file:
+            while chunk := file.read(8192):
+                if time.time() - start_time > namespace.timeout:
+                    await proc.kill()
+                    if namespace.logging:
+                        logger.error('Download was interrupted')
+                    raise asyncio.CancelledError
+                await response.write(chunk)
+                if namespace.logging:
+                    logger.info('Sending archive chunk ...')
+        await response.write_eof()
+    except TimeoutError:
+        logger.error('Download was interrupted')
+    finally:
+        os.remove(zip_path)
+
+    return response
 
 
 async def handle_index_page(request):
@@ -13,6 +55,27 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-t',
+        '--timeout',
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        '-l',
+        '--logging',
+        type=bool,
+        default=False
+    )
+    parser.add_argument(
+        '-p',
+        '--path',
+        type=str,
+        default='test_photos/'
+    )
+    namespace = parser.parse_args()
+    logger = Logger.with_default_handlers(name='write_chunk_logger')
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
